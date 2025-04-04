4cac6d461990ada0ce995be30dcc373d9f22383e
Pedro Ferraz <pvf@cesar.school>
2025-04-04 20:55:54 -0300

    cadastre-se

--- Interface/urls.py
+++ Interface/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', LoginView.as_view(template_name='static/login.html'), name='login'),
+    path('cadastrar/', views.cadastrar, name='cadastrar')
 ]
