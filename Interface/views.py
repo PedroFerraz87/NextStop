a6190dc0f150428009655f517674e1c98c5a9bc0
Pedro Ferraz <pvf@cesar.school>
2025-04-07 17:28:05 -0300

    Update views.py

--- Interface/views.py
+++ Interface/views.py
@@ -4,22 +4,22 @@ from django.shortcuts import redirect, render
 from .models import Roteiro
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'Interface/home.html')
 
 def cadastro(request):
-    return render(request, 'cadastro.html')
+    return render(request, 'Interface/cadastro.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'Interface/login.html')
 
 def roteiro(request):
-    return render(request, 'roteiro.html')
+    return render(request, 'Interface/roteiro.html')
 
 def orçamento(request):
-    return render(request, 'orçamento.html')
+    return render(request, 'Interface/orçamento.html')
 
 def sugestão(request):
-    return render(request, 'sugestão.html')
+    return render(request, 'Interface/sugestão.html')
 
 
 
