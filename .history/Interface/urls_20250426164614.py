275bd16b5e107dfbc6aeb38724bbc9cb6b75055d
Pedro Ferraz <pvf@cesar.school>
2025-04-26 19:12:42 -0300

    diversas atualizações

---
+++ .history/Interface/urls_20250426164614.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from .views import logout_view
+
+
+urlpatterns = [
+    path('', views.login_view, name='login'),
+    path('home/', views.home, name='home'),
+    path('cadastro/', views.cadastro, name='cadastro'),
+    path('logout/', logout_view, name='logout'),
+    path('roteiro/', views.roteiro, name='roteiro'),
+    path('orçamento/', views.orçamento, name='orçamento'),
+    path('sugestão/', views.sugestão, name='sugestão'),
+    path('checklist/', views,checklist, name='checklist')
+]
